f8003a8e36bfa49ab0494d1203521f5cdf310a48
dimasneto7 <dimas_fcn@outlook.com>
2025-07-18 23:27:44 -0300

    profile form

--- src/app/(panel)/dashboard/profile/_components/profile.tsx
+++ src/app/(panel)/dashboard/profile/_components/profile.tsx
@@ -20,12 +20,114 @@ import {
 import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
 import { Label } from '@/components/ui/label'
+import Image from 'next/image'
+import imgTest from '../../../../../../public/foto1.png'
 
 export function ProfileContent() {
   const form = useProfileForm()
   return (
-    <div>
-      <h1>Profile Content</h1>
+    <div className="mx-auto">
+      <Form {...form}>
+        <form>
+          <Card>
+            <CardHeader>
+              <CardTitle>Meu Perfil</CardTitle>
+            </CardHeader>
+            <CardContent className="space-y-6">
+              <div className="flex justify-center">
+                <div className="bg-gray-200 relative w-40 h-40 rounded-full overflow-hidden">
+                  <Image
+                    src={imgTest}
+                    alt="Foto da Clínica"
+                    fill
+                    className="object-cover"
+                  />
+                </div>
+              </div>
+              <div className="space-y-4">
+                <FormField
+                  control={form.control}
+                  name="name"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel className="font-semibold">
+                        Nome completo
+                      </FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder="Digite o nome da Clínica..."
+                          {...field}
+                        />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
+
+                <FormField
+                  control={form.control}
+                  name="address"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel className="font-semibold">
+                        Endereço Completo
+                      </FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder="Digite o endereço da Clínica..."
+                          {...field}
+                        />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
+
+                <FormField
+                  control={form.control}
+                  name="phone"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel className="font-semibold">Telefone</FormLabel>
+                      <FormControl>
+                        <Input placeholder="Digite o telefone..." {...field} />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
+
+                <FormField
+                  control={form.control}
+                  name="status"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel className="font-semibold">
+                        Status da Clínica
+                      </FormLabel>
+                      <FormControl>
+                        <Select
+                          onValueChange={field.onChange}
+                          defaultValue={field.value ? 'active' : 'inactive'}
+                        >
+                          <SelectTrigger>
+                            <SelectValue placeholder="Selecione o status da Clínica" />
+                          </SelectTrigger>
+                          <SelectContent>
+                            <SelectItem value="active">
+                              ATIVO - Clínica aberta
+                            </SelectItem>
+                            <SelectItem value="inactive">
+                              INATIVO - Clínica fechada
+                            </SelectItem>
+                          </SelectContent>
+                        </Select>
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
+              </div>
+            </CardContent>
+          </Card>
+        </form>
+      </Form>
     </div>
   )
 }
